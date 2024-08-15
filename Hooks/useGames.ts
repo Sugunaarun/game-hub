f46047d742736866b737ab5e253581daea90551a
Arun <arunnishanthan16@gmail.com>
2024-08-15 16:10:38 +0800

    GameCard

--- Hooks/useGames.ts
+++ Hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import apiClient from "../src/Service.ts/api-client";
 import { CanceledError } from "axios";
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGameResponse {
