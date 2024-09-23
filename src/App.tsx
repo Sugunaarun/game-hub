f7ffab1dd942597c932e7c027f433cc70952e337
Arun <arunnishanthan16@gmail.com>
2024-09-23 16:06:27 +0800

    Searching Games

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
   sortOrder: string;
+  searchValue: string;
 }
 
 function App() {
@@ -30,7 +31,11 @@ function App() {
       }}
     >
       <GridItem area="nav">
-        <NavBar />
+        <NavBar
+          onSearch={(searchValue) =>
+            setGameQuery({ ...gameQuery, searchValue })
+          }
+        />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
